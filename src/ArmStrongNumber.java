90494c0a81afb304e8db7e7f50424ec8aae7d208
Shubham Bharti <shubhambharti04@gmail.com>
2022-10-02 13:18:15 +0530

    ArmStrongNumber

---
+++ src/ArmStrongNumber.java
@@ -0,0 +1,18 @@
+public class ArmStrongNumber {
+    public static void main(String[] main){
+        int number=37;
+        int OriginalNumber=number;
+        int reminder=0;
+        int result=0;
+        while(OriginalNumber!=0){
+           reminder=OriginalNumber%10;
+           result += (int) Math.pow(reminder,3);
+           OriginalNumber=OriginalNumber/10;
+
+        }
+        if(result == number)
+            System.out.println(number + " is an Armstrong number.");
+        else
+            System.out.println(number + " is not an Armstrong number.");
+    }
+}
