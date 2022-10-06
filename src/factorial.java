c342f4dc791bc26c883ad864e98e47deaec815b0
Shubham Bharti <shubhambharti04@gmail.com>
2022-10-06 12:38:36 +0530

    factorial corrected

--- src/factorial.java
+++ src/factorial.java
@@ -1,16 +1,13 @@
 public class factorial {
     public static int fact(int n){
-        int result=0;
+
         if(n==0){
-            return 0;
-        }
-        if(n==1){
             return 1;
         }
-        if(n>1){
-            result = n*fact(n-1) ;
+        else {
+            return n*fact(n-1) ;
         }
-        return  result;
+
     }
 
   public static void main(String[] args){
