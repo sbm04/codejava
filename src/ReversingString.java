0f7a64482b3bbd13872a20d0f51cf5464d535b36
Shubham Bharti <shubhambharti04@gmail.com>
2022-10-06 13:30:18 +0530

    ReversingString using string buffer

--- src/ReversingString.java
+++ src/ReversingString.java
@@ -1,10 +1,17 @@
 public class ReversingString {
     public static void main(String[] args){
-        String str ="Hello";
-        String newStr = "";
-        for(int i=(str.length()-1);i>=0;i--){
-            newStr += str.charAt(i);
-        }
-        System.out.println(newStr);
+//        String str ="Hello";
+//        String newStr = "";
+//        for(int i=(str.length()-1);i>=0;i--){
+//            newStr += str.charAt(i);
+//        }
+//        System.out.println(newStr);
+        String str = "Geeks";
+
+        // conversion from String object to StringBuffer
+        StringBuffer sbr = new StringBuffer(str);
+        // To reverse the string
+        sbr.reverse();
+        System.out.println(sbr);
     }
 }
