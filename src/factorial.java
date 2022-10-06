117826975b868a6b758463872c0599c0098c816c
Shubham Bharti <shubhambharti04@gmail.com>
2022-10-06 12:37:03 +0530

    factorial by me

--- src/factorial.java
+++ src/factorial.java
@@ -1,6 +1,21 @@
 public class factorial {
+    public static int fact(int n){
+        int result=0;
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        if(n>1){
+            result = n*fact(n-1) ;
+        }
+        return  result;
+    }
 
   public static void main(String[] args){
-      System.out.println("Factorial");
+
+      int n=5;
+      System.out.println(fact(8));
   }
 }
