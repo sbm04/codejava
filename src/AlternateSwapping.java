f029593707c477667191833316000a13be36367f
Shubham Bharti <shubhambharti04@gmail.com>
2022-10-08 13:00:12 +0530

    Alternate Swapping

---
+++ src/AlternateSwapping.java
@@ -0,0 +1,13 @@
+public class AlternateSwapping {
+
+    public static void swapAlternate(int arr[]) {
+        //Your code goes here
+
+        for(int i=0;i<(arr.length-1);i+=2){
+            int temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+        }
+    }
+
+}
