43334b403451eaac7bfb81fd1cca1d16891160b6
Shubham Bharti <shubhambharti04@gmail.com>
2022-10-09 01:15:47 +0530

    intersection Of Two Array

---
+++ src/intersectionOfTwoArray.java
@@ -0,0 +1,15 @@
+public class intersectionOfTwoArray {
+    public static void intersections(int arr1[], int arr2[]) {
+        //Your code goes here
+        for(int i=0;i<arr1.length;i++){
+            int j;
+            for(j=0;j<arr2.length;j++){
+                if(arr1[i]==arr2[j]){
+                    System.out.print(arr1[i]+" ");
+                    arr2[j]=Integer.MAX_VALUE;
+                    break;
+                }
+            }
+        }
+    }
+}
