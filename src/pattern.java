0a07bbcd92f52d4035af250d5b263e01a2db4f7b
Shubham Bharti <shubhambharti04@gmail.com>
2022-10-23 10:10:16 +0530

    pattern 1 121 1221 12221

---
+++ src/pattern.java
@@ -0,0 +1,18 @@
+public class pattern {
+    public static void main(String[] args) {
+        int start =1;
+        System.out.println(start);
+        int mid =2;
+
+
+        for(int i=0;i<3;i++){
+            System.out.print(start);
+            for(int j=0;j<=i;j++){
+                System.out.print(mid);
+
+            }
+                System.out.print(start);
+                System.out.println();
+        }
+    }
+}
