1bb0d45ee3a1fed1500f5597a7bdd211298b4e69
Shubham Bharti <shubhambharti04@gmail.com>
2022-10-24 13:49:44 +0530

    Book Map

---
+++ src/collection/map/bookMap.java
@@ -0,0 +1,36 @@
+package collection.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Book{
+    int id;
+    String name,author,publisher;
+    int quantity;
+    public Book(int id, String name, String author, String publisher, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.publisher = publisher;
+        this.quantity = quantity;
+    }
+}
+public class bookMap {
+    public static void main(String[] args) {
+        HashMap<Integer, Book> bm = new HashMap<Integer, Book>();
+        Book b1= new Book(101,"Let us C","Yashwant Kanetkar","BPB",8);
+        Book b2=new Book(102,"Data Communications & Networking","Forouzan","Mc Graw Hill",4);
+        Book b3=new Book(103,"Operating System","Galvin","Wiley",6);
+        //Adding Books to map
+        bm.put(1,b1);
+        bm.put(2,b2);
+        bm.put(3,b3);
+       for(Map.Entry<Integer,Book> entry: bm.entrySet()){
+           int key = entry.getKey();
+           Book b=entry.getValue();
+           System.out.println(key + " Detail: ");
+           System.out.println(b.id+" "+b.name+" "+b.author+" "+b.publisher+" "+b.quantity);
+
+       }
+    }
+}
