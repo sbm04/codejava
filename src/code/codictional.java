9474bbcc15bab70db54c1618f17e5336c8e30061
Shubham Bharti <shubhambharti04@gmail.com>
2023-06-01 23:47:30 +0530

    basic

---
+++ src/code/codictional.java
@@ -0,0 +1,31 @@
+package code;
+
+import java.util.Scanner;
+
+public class codictional {
+    public static void main(String[] args) {
+        // input - don't change this code
+        Scanner scn = new Scanner(System.in);
+        int marks = scn.nextInt();
+        // input - don't change this code
+
+        // code here
+        if(marks>90){
+            System.out.println("excellent");
+        }
+        else if(marks>80 && marks<=90){
+            System.out.println("good");
+        }
+        else if(marks>70 && marks<=80){
+            System.out.println("fair");
+        }
+        else if(marks>60 && marks<=70){
+            System.out.println("meets expectation");
+        }
+        else if(marks<=60 ){
+            System.out.println("below par");
+        }
+
+    }
+}
+
